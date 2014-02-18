14c380e2235fb8b723d10a8ca45bd93c8c58e7e6
Geo Miller <geoffery.miller@gmail.com>
2014-02-18 13:52:26 -0500

    debugging 302s from some services

--- ac.js
+++ ac.js
@@ -5,9 +5,14 @@ var htmlparser = require('htmlparser2')
 function download(url, callback) {
   url = urlparser.parse(url)
   http.get({hostname:url.hostname, path:url.path}, function(res) {
+    console.log("Status: " + res.statusCode)
+    console.log(res.headers.location)
     data = ""
     res.on('data', function(chunk) {
       data += chunk
+    })
+    res.on('error', function(e) {
+      console.log(e.message)
     })
     res.on('end', function() {
       callback(data)
@@ -19,13 +24,14 @@ function ac(url) {
   download(url, function(data) {
     var parse = new htmlparser.Parser({
       onopentag: function (name, attribs) {
-        if (name === "link" && attribs.type === "text/css") {
+        if (name === "link" && (attribs.rel === "stylesheet" || attribs.type === "text/css")) {
           console.log(attribs.href);
         }
       }
     })
     parse.write(data)
     parse.end()
+    console.log(data)
   })
 }
 
