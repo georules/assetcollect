9721ed354b447243321e7806448a0db8d026c2ef
Geo Miller <geoffery.miller@gmail.com>
2014-02-18 21:55:40 -0500

    using promises to download files

--- ac.js
+++ ac.js
@@ -1,51 +1,75 @@
 var http = require('http');
 var urlparser = require('url');
 var htmlparser = require('htmlparser2')
+var Promise = require('promise')
 
-function download(url, callback) {
-  url = urlparser.parse(url)
-  http.get({hostname:url.hostname, path:url.path}, function(res) {
-    console.log("Status: " + res.statusCode)
-    console.log(res.headers.location)
-    data = ""
-    res.on('data', function(chunk) {
-      data += chunk
-    })
-    res.on('error', function(e) {
-      console.log(e.message)
-    })
-    res.on('end', function() {
-      callback(data)
+function download(url) {
+  var promise = new Promise(function (resolve, reject) {
+    parsedurl = urlparser.parse(url)
+    http.get(parsedurl, function(res) {
+      data = ""
+      res.on('data', function(chunk) {
+        data += chunk
+      })
+      res.on('error', function(e) {
+        console.log(e.message)
+      })
+      res.on('end', function() {
+        resolve(data)
+      })
     })
   })
+  return promise;
 }
 
 function ac(url) {
-  download(url, function(data) {
+  cssdata = []
+  todownload = []
+  csspos = 0
+  download(url).then(function(data) {
     var instyle = false;
+    var incsspos
     var parse = new htmlparser.Parser({
       onopentag: function (name, attribs) {
         if (name === "link" && (attribs.rel === "stylesheet" || attribs.type === "text/css")) {
-          console.log(attribs.href);
+          thisurl = ""
+          if(attribs.href.indexOf("http") >= 0) thisurl = attribs.href
+          else thisurl = url+"/"+attribs.href
+          todownload[csspos] = thisurl;
+          csspos += 1
         }
         if (name === "style") {
-          console.log("{{on-page style data}}")
-          instyle = true;
+          cssdata[csspos] = "/* {{on-page style data}} */\n"
+          incsspos = csspos
+          csspos += 1
         }
       },
       ontext: function(data) {
-        if (instyle)  {
-          console.log(data)
+        if (incsspos > 0)  {
+          cssdata[incsspos] += data
         }
       },
       onclosetag: function(name)  {
         if(name==="style")  {
-          instyle = false;
+          incsspos = 0;
         }
-      }
+      },
     })
     parse.write(data)
     parse.end()
+  }).then(function() {
+
+    var promises = []
+    todownload.forEach(function(url,i) {
+      promises.push(download(url).then(function(data){
+        cssdata[i] = "/* {{"+url+"}} */\n" + data
+      }))
+    })
+    Promise.all(promises).then(function() {
+      cssdata.forEach(function(data){
+        console.log(data+"\n")
+      })
+    })
   })
 }
 
